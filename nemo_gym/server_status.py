296526d134aafece6d40bfe4f2f4094853416ef1
Frankie Siino <fsiino@nvidia.com>
2025-11-10 15:56:58 -0800

    Parse server info
    Signed-off-by: Frankie Siino <fsiino@nvidia.com>

--- nemo_gym/server_status.py
+++ nemo_gym/server_status.py
@@ -11,11 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from time import time
 from typing import List, Optional
 
 import psutil
+from omegaconf import OmegaConf
 from pydantic import BaseModel
 
+from nemo_gym.global_config import NEMO_GYM_CONFIG_DICT_ENV_VAR_NAME, NEMO_GYM_CONFIG_PATH_ENV_VAR_NAME
 from nemo_gym.server_utils import ServerStatus
 
 
@@ -35,14 +38,61 @@ class ServerProcessInfo(BaseModel):
 
 
 def parse_server_info(proc, cmdline: List[str], env: dict) -> Optional[ServerProcessInfo]:
-    """TODO: Parse server info"""
-    pass
+    """Takes process, command line, and env and returns server process information"""
+    try:
+        process_name = env.get(NEMO_GYM_CONFIG_PATH_ENV_VAR_NAME)
+        if not process_name:
+            return None
+
+        config_dict_yaml = env.get(NEMO_GYM_CONFIG_DICT_ENV_VAR_NAME)
+        if not config_dict_yaml:
+            return None
+
+        global_config_dict = OmegaConf.create(config_dict_yaml)
+
+        # TODO: implement getting specific server config
+        server_config_dict = {}
+
+        top_level_config = global_config_dict[process_name]
+        server_type = list(top_level_config.keys())[0]
+        server_type_config = top_level_config[server_type]
+        server_name = list(server_type_config.keys())[0]
+
+        host = server_config_dict.get("host")
+        port = server_config_dict.get("port")
+
+        url = f"http://{host}:{port}" if host and port else None
+
+        entrypoint = None
+        for i, arg in enumerate(cmdline):
+            if arg == "python" and i + 1 < len(cmdline):
+                entrypoint = cmdline[i + 1]
+                break
+
+        current_time = time.time()
+        create_time = proc.info["create_time"]
+        uptime_seconds = current_time - create_time
+
+        return ServerProcessInfo(
+            pid=proc.info["pid"],
+            server_type=server_type,
+            name=server_name,
+            process_name=process_name,
+            host=host,
+            port=port,
+            url=url,
+            uptime_seconds=uptime_seconds,
+            status=ServerStatus.RUNNING,  # TODO: implement status checking
+            entrypoint=entrypoint,
+        )
+    except (KeyError, IndexError, AttributeError, TypeError):
+        return None
 
 
 class StatusCommand:
     """Main class to check server status"""
 
-    def discover_servers() -> List[ServerProcessInfo]:
+    def discover_servers(self) -> List[ServerProcessInfo]:
         """Find all running NeMo Gym server processes"""
         servers = []
         for proc in psutil.process_iter(["pid", "name", "cmdline", "create_time", "environ"]):
@@ -61,3 +111,10 @@ class StatusCommand:
                 continue
 
         return servers
+
+    def display_status(self, servers: List[ServerProcessInfo]) -> None:
+        """Show server info in a table"""
+        # TODO: flesh out
+        if not servers:
+            print("No NeMo Gym servers found running.")
+            return
